b5871e52600d12cf6447fcf91d2cc4e665fa36ab
entrylevelcs <54680246+entrylevelcs@users.noreply.github.com>
2019-09-29 22:04:15 -0600

    Create Activity_4.js

---
+++ Activity_4.js
@@ -0,0 +1,31 @@
+let hand = 4
+input.onGesture(Gesture.Shake, function () {
+    hand = Math.randomRange(0, 2)
+    if (hand == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    else if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    else if (hand == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
